684a82aa4f5e0e2672a48ddab55298ba820e4cb5
pambuk <wojtek.zymonik@gmail.com>
2017-12-14 20:29:30 +0100

    - formatting, - docblocks, - added missing api_token to migrations

--- app/OperationItem.php
+++ app/OperationItem.php
@@ -6,6 +6,31 @@ use App\Items\VolumeDisplay;
 use App\Items\WeightDisplay;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\OperationItem
+ *
+ * @property int $id
+ * @property int $operation_id
+ * @property string $description
+ * @property int $value
+ * @property int $quantity
+ * @property float|null $volume
+ * @property int|null $weight
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read mixed $volume_weight
+ * @property-read \App\Operation $operation
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereOperationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereQuantity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereValue($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereVolume($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationItem whereWeight($value)
+ * @mixin \Eloquent
+ */
 class OperationItem extends Model
 {
     protected $fillable = ['operation_id', 'description', 'value', 'volume', 'weight', 'quantity',];
--- app/OperationSource.php
+++ app/OperationSource.php
@@ -4,6 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\OperationSource
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string $visibility
+ * @property string|null $default_operation_type
+ * @property int|null $owner_id
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationSource whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationSource whereDefaultOperationType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationSource whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationSource whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationSource whereOwnerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationSource whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\OperationSource whereVisibility($value)
+ * @mixin \Eloquent
+ */
 class OperationSource extends Model
 {
     const VISIBILITY_PRIVATE = 'private';
